3926525d73e9b7193e1bde605f1e8beaaf2739f6
StrajnarFilip <46705237+StrajnarFilip@users.noreply.github.com>
2022-11-19 12:42:49 +0000

    Update time.ps1

--- time.ps1
+++ time.ps1
@@ -2,10 +2,11 @@ Start-Transcript -Path "$PSScriptRoot\transcript.txt"
 
 while ($true) {
     try {
-        $rawJson = $(Invoke-WebRequest -Uri "https://clock.zone/_/_offset" -UseBasicParsing).Content
-        $unixEpoch = [System.Text.Json.JsonDocument]::Parse($rawJson).RootElement.GetProperty("val").GetInt64()
-        $accurateDateTime = ([System.DateTimeOffset]::FromUnixTimeMilliseconds($unixEpoch)).LocalDateTime
+        $rawJson = $(Invoke-WebRequest -Uri "https://worldtimeapi.org/api/timezone/Europe/London" -UseBasicParsing).Content
+        $datetimeString = [System.Text.Json.JsonDocument]::Parse($rawJson).RootElement.GetProperty("datetime").GetString()
+        $accurateDateTime = ([System.DateTime]::Parse($datetimeString))
         Set-Date -Date $accurateDateTime
+        Add-Content -Value $accurateDateTime -Path "$PSScriptRoot\log.txt"
     }
     catch {}
     Start-Sleep -Seconds 60
